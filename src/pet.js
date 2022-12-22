114788e197935f7167564b900047a593ae57cef4
Ernesto Angustia <ernesto.angustia@gmail.com>
2022-12-22 22:56:56 +0000

    VirtualPet

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,14 @@
 function Pet(name) {
     this.name = name;
-}
+    this.age = 0;
+    this.fitness = 10;
+    this.hunger = 0;
+    }
+    Pet.prototype.growUp = function() {
+      this.age += 1;
+      this.hunger += 5;
+      this.fitness -= 3;
+    };
 
 module.exports = Pet;
+
